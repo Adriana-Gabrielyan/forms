e8c1f4b68cd8e257b340fdbd34f35a8bca3da428
Adriana-Gabrielyan <adrin.gabrielyan1997@gmail.com>
2021-08-02 16:49:34 +0400

    add styles and correcct functionality

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MustMatch } from '../_helpers/match.validator';
 
 @Component({
   selector: 'app-form',
@@ -13,43 +9,88 @@ import {
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-  userForm: FormGroup;
+  usersForm!: FormGroup;
+  allowToAdd: boolean = false;
 
-  constructor(private fb: FormBuilder) {
-    this.userForm = this.fb.group({
-      credentials: this.fb.array([]),
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.usersForm = this.fb.group({
+      users: this.fb.array([
+        this.fb.group(
+          {
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.minLength(3)]],
+            email: [
+              '',
+              [
+                Validators.required,
+                Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+              ],
+            ],
+            zip: ['', [Validators.required, Validators.maxLength(10)]],
+            password: [
+              '',
+              [
+                Validators.required,
+                Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+              ],
+            ],
+            confirm_password: [
+              '',
+              [Validators.required, Validators.minLength(6)],
+            ],
+          },
+          {
+            validator: MustMatch('password', 'confirm_password'),
+          }
+        ),
+      ]),
     });
-    this.addForm();
   }
-  trackByFn(index: any, item: any) {
-    return index;
+  get userFormGroups() {
+    return this.usersForm.get('users') as FormArray;
   }
+
+  getValidity(i: any) {
+    return (<FormArray>this.usersForm.get('users')).controls[i].invalid;
+  }
+
+  deleteForm(i: any) {
+    let usersArray = this.usersForm.controls.users as FormArray;
+    usersArray.removeAt(i);
+  }
+
   addForm() {
-    const creds = this.userForm.controls.credentials as FormArray;
-    creds.push(
-      this.fb.group({
-        firstName: ['', [Validators.required, Validators.minLength(3)]],
-        lastName: ['', [Validators.required, Validators.minLength(3)]],
-        email: [
-          '',
-          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+    let usersArray = this.usersForm.controls.users as FormArray;
+    let arraylen = usersArray.length;
+
+    let newUsergroup: FormGroup = this.fb.group({
+      firstName: ['', [Validators.minLength(3), Validators.required]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')],
+      zip: ['', [Validators.required, Validators.maxLength(10)]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
         ],
-        zip: ['', [Validators.required, Validators.maxLength(10)]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        confirm_password: ['', [Validators.required, Validators.minLength(6)]],
-      })
-    );
+      ],
+      confirm_password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+    this.allowToAdd = true;
+    if (!this.getValidity(arraylen - 1)) {
+      if (arraylen < 10) {
+        usersArray.insert(arraylen, newUsergroup);
+      } else {
+        alert("You can't add more than 10 forms");
+      }
+    }
   }
-  deleteForm(index: any) {
-    console.log(index);
-  }
-  get credentials() {
-    return this.userForm.get('credentials');
-  }
-
   onSubmit() {
-    console.log(this.userForm);
+    if (!this.usersForm.invalid) {
+      console.log(this.usersForm.value);
+    }
   }
-
-  ngOnInit(): void {}
 }
